2887716bf9dd0a79bfbd2b12d8d9edad9faf629c
kwame2 <softcons99@yahoo.com>
2015-08-20 09:54:24 +0200

    first commit

---
+++ src/main/java/com/robert/bdcyclists/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.robert.bdcyclists;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BdcbookApplication.class);
+	}
+
+}
